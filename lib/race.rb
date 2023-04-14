d66c1682df845c92a5283083e7c3a42928298149
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-04-14 10:51:46 -0600

    feat: add open? method

--- lib/race.rb
+++ lib/race.rb
@@ -1,13 +1,16 @@
 class Race
-  attr_reader :office, :candidates
-  def initialize(office)
+  attr_reader :office, :candidates, :open
+  def initialize(office, open = true)
     @office = office
     @candidates = []
+    @open = open
   end
 
   def register_candidate!(candidate_info)
     candidates << candidate_info
   end
-end
 
-  
+  def open?
+    @open
+  end
+end
