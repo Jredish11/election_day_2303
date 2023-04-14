72849b193449d5c435af7d0fabdb32c400cd4d59
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-04-14 10:04:27 -0600

    test: add tests for list of election candidates

--- spec/election_spec.rb
+++ spec/election_spec.rb
@@ -22,8 +22,21 @@ RSpec.describe Election do
     it "adds a race to the list of election races" do
       election = Election.new("2000")
       governor = Race.new("Texas Governor")
+      mayor = Race.new("Denver Mayor")
       election.add_race(governor)
-      expect(election.races).to eq([governor])
+      election.add_race(mayor)
+      expect(election.races).to eq([governor, mayor])
+    end
+
+    it "has a list of all candidates in the election" do
+      election = Election.new("2000")
+      governor = Race.new("Texas Governor")
+      candidate1 = Candidate.new({name: "Diana D", party: :democrat})
+      candidate2 = Candidate.new({name: "Roberto R", party: :republican})
+      election.add_race(governor)
+      governor.register_candidate!(candidate1)
+      governor.register_candidate!(candidate2)
+      expect(election.candidates).to eq([candidate1, candidate2])
     end
   end
 end
@@ -38,9 +51,6 @@ end
 
 
 
-#new	An Election object that has an attribute of year (string data type).
-#year	The year of the election (string).
-#races	An array of Race objects.
-#add_race(race)	An array of Race objects.
-#candidates	An array of all Candidate objects in the election.
+
+
 #vote_counts	A hash with each candidate's name as a key and their count of votes as the value.
