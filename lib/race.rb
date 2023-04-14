fb771736f16644992d60d1420f68f94468fa2e8e
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-04-14 09:34:21 -0600

    feat: Add initialize and register_candidate method

---
+++ lib/race.rb
@@ -0,0 +1,11 @@
+class Race
+  attr_reader :office, :candidates
+  def initialize(office)
+    @office = office
+    @candidates = []
+  end
+
+  def register_candidate!(candidate_info)
+    candidates << Candidate.new(candidate_info)
+  end
+end
