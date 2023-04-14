8ac0843645bc7ba2007a66f2527fabcd43e129f8
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-04-14 09:37:42 -0600

    test: Add rest of tests for register candidates

--- spec/race_spec.rb
+++ spec/race_spec.rb
@@ -21,10 +21,13 @@ RSpec.describe Race do
     it "registers a candidate" do
       race = Race.new("Texas Governor")
       candidate1 = Candidate.new({name: "Diana D", party: :democrat})
-      race.register_candidate!({name: "Diana D", party: :democrat})
+      race.register_candidate!(candidate1)
       expect(candidate1.class).to eq(Candidate)
       expect(candidate1.name).to eq("Diana D")
       expect(candidate1.party).to eq(:democrat)
+      candidate2 = Candidate.new({name: "Roberto R", party: :republican})
+      race.register_candidate!(candidate2)
+      expect(race.candidates).to eq([candidate1, candidate2])
     end
 
 
@@ -40,20 +43,3 @@ end
 
 
 
-#  candidate1 = race.register_candidate!({name: "Diana D", party: :democrat})
-# => #<Candidate:0x00007f9edf376c90...>
-
-#  candidate1.class
-# => Candidate
-
-#  candidate1.name
-# => "Diana D"
-
-#  candidate1.party
-# => :democrat
-
-#  candidate2 = race.register_candidate!({name: "Roberto R", party: :republican})
-# => #<Candidate:0x00007f9edf386780...>
-
-#  race.candidates
-# => [#<Candidate:0x00007f9edf376c90...>, #<Candidate:0x00007f9edf386780...>]
