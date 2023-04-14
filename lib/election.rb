ed868ac0d15cd64ab1f6765a711acbd31b0d889f
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-04-14 09:55:02 -0600

    feat: add_race(race) method git status

--- lib/election.rb
+++ lib/election.rb
@@ -1,6 +1,11 @@
 class Election
-  attr_reader :year
+  attr_reader :year, :races
   def initialize(year)
     @year = year
+    @races = []
+  end
+
+  def add_race(race)
+    races << race
   end
 end
